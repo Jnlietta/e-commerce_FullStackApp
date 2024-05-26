ac6441cf474a1a70966b1c515e77311a942c916c
Julia Glinka <julia.w.glinka@gmail.com>
2024-05-26 22:18:08 +0200

    Add create-order and update-order DTOs files

---
+++ src/orders/dtos/update-order.dto.ts
@@ -0,0 +1,40 @@
+import {
+  IsNotEmpty,
+  IsString,
+  IsInt,
+  Min,
+  ArrayNotEmpty,
+  IsEmail,
+} from 'class-validator';
+
+export class CreateOrderDTO {
+  @IsNotEmpty()
+  @IsString()
+  userName: string;
+
+  @IsNotEmpty()
+  @IsEmail()
+  userEmail: string;
+
+  @IsNotEmpty()
+  @IsString()
+  userAddress: string;
+
+  @ArrayNotEmpty()
+  @IsString({ each: true })
+  products: string[];
+
+  @IsNotEmpty()
+  @IsString()
+  delivery: string;
+
+  @IsNotEmpty()
+  @IsInt()
+  @Min(0)
+  priceOnlyProducts: number;
+
+  @IsNotEmpty()
+  @IsInt()
+  @Min(0)
+  priceTotal: number;
+}
