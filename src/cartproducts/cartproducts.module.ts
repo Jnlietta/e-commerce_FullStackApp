189ad38cc9c19612e8d320ce47ed1c5da91fc775
Julia Glinka <julia.w.glinka@gmail.com>
2024-05-27 21:45:51 +0200

    Add PrismaService in cartproducts.module

--- src/cartproducts/cartproducts.module.ts
+++ src/cartproducts/cartproducts.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { CartproductsController } from './cartproducts.controller';
 import { CartproductsService } from './cartproducts.service';
+import { PrismaService } from 'src/shared/services/prisma.service';
 
 @Module({
   controllers: [CartproductsController],
-  providers: [CartproductsService]
+  providers: [CartproductsService, PrismaService],
 })
 export class CartproductsModule {}
