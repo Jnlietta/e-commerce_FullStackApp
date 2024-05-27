2e1693a268b54e9dbad226a100a229cb7d75026d
Julia Glinka <julia.w.glinka@gmail.com>
2024-05-27 23:22:35 +0200

    Add endpoints GET, POST in cartproducts.controller

--- src/cartproducts/cartproducts.controller.ts
+++ src/cartproducts/cartproducts.controller.ts
@@ -1,7 +1,18 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Post } from '@nestjs/common';
 import { CartproductsService } from './cartproducts.service';
+import { CartProduct } from '@prisma/client';
 
 @Controller('cartproducts')
 export class CartproductsController {
   constructor(private cartproductsService: CartproductsService) {}
+
+  @Get('/')
+  getAll() {
+    return this.cartproductsService.getAll();
+  }
+
+  @Post('/')
+  create(cartProductData: Omit<CartProduct, 'id'>) {
+    return this.cartproductsService.create(cartProductData);
+  }
 }
