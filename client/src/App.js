a4eecf3ec8cc3f62f7ea5badc78a584c106c61a7
Julia Glinka <julia.w.glinka@gmail.com>
2024-05-29 12:51:23 +0200

    Add Header, Footer, Container in App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,9 @@
 import { Routes, Route } from 'react-router-dom';
+import { Container } from 'react-bootstrap';
+import Header from './components/views/Header/Header';
+import Footer from './components/views/Footer/Footer';
+
+// import routes
 import Home from './components/pages/Home/Home';
 import Product from './components/pages/Product/Product';
 import Cart from './components/pages/Cart/Cart';
@@ -6,13 +11,17 @@ import Order from './components/pages/Order/Order';
 
 const App = () => {
   return (
-    <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/product/:id" element={<Product />} />
-      <Route path="/cart" element={<Cart />} />
-      <Route path="/order" element={<Order />} />
-      <Route path="*" element={<Home />} />
-    </Routes>
+    <Container>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/product/:id" element={<Product />} />
+        <Route path="/cart" element={<Cart />} />
+        <Route path="/order" element={<Order />} />
+        <Route path="*" element={<Home />} />
+      </Routes>
+      <Footer />
+    </Container>
   );
 };
 
