b1c2a1eb3a3ea8f8517b36a1f0220c07fee3447f
Julia Glinka <julia.w.glinka@gmail.com>
2024-05-29 12:35:10 +0200

    Add Rotes in App.js and folders for each page

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,18 @@
+import { Routes, Route } from 'react-router-dom';
+import Home from './components/pages/Home/Home';
+import Product from './components/pages/Product/Product';
+import Cart from './components/pages/Cart/Cart';
+import Order from './components/pages/Order/Order';
+
 const App = () => {
   return (
-    <h1>Hello world</h1>
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/product/:id" element={<Product />} />
+      <Route path="/cart" element={<Cart />} />
+      <Route path="/order" element={<Order />} />
+      <Route path="*" element={<Home />} />
+    </Routes>
   );
 };
 
