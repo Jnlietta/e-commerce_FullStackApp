c6118b5aada4426faf449953a6add6c08bbb74d6
Julia Glinka <julia.w.glinka@gmail.com>
2024-05-29 21:54:29 +0200

    Add public/images, background photo and styles in Header.js

--- client/src/components/views/Header/Header.js
+++ client/src/components/views/Header/Header.js
@@ -1,9 +1,11 @@
 import NavBar from "../NavBar/NavBar";
 import TopBar from "../TopBar/TopBar";
+import styles from './Header.module.scss';
 
 const Header = props => {
     return(
-        <div>
+        <div className={styles.headerContainer}>
+            <div className={styles.backgroundOverlay}></div>
             <TopBar />
             <NavBar />
         </div>
