0ebe95aece98d74077d6449b05ac741be4c76f20
Julia Glinka <julia.w.glinka@gmail.com>
2024-05-29 20:07:27 +0200

    Add hamburger for links and styles in NavBar

--- client/src/components/views/NavBar/NavBar.js
+++ client/src/components/views/NavBar/NavBar.js
@@ -2,6 +2,8 @@ import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import { NavLink } from 'react-router-dom';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faBars } from '@fortawesome/free-solid-svg-icons';
 import clsx from 'clsx';
 import styles from './NavBar.module.scss';
 
@@ -10,12 +12,18 @@ const NavBar = ({ isLoggedIn }) => {
         <Navbar expand="lg" className={clsx('mb-4', styles['navbar-transparent'])}>
             <Container>
                 <Navbar.Brand href="#home" className={styles.navbarBrand}>SWAY.</Navbar.Brand>
-                <Nav className="justify-content-end">
-                    <Nav.Link as={NavLink} to="/">Home</Nav.Link>
-                    {!isLoggedIn && <Nav.Link as={NavLink} to="/user/register">Sign in</Nav.Link>}
-                    {!isLoggedIn && <Nav.Link as={NavLink} to="/user/login">Sign up</Nav.Link>}
-                    {isLoggedIn && <Nav.Link as={NavLink} to="/user/logout">Sign out</Nav.Link>}
-                </Nav>
+                
+                <Navbar.Toggle aria-controls="basic-navbar-nav">
+                    <FontAwesomeIcon className={styles.icon} icon={faBars} />
+                </Navbar.Toggle>
+                <Navbar.Collapse id="basic-navbar-nav" className={styles.navbarCollapse}>
+                    <Nav>
+                        <Nav.Link as={NavLink} to="/">Home</Nav.Link>
+                        <Nav.Link as={NavLink} to="/clothes">Clothes</Nav.Link>
+                        <Nav.Link as={NavLink} to="/accessories">Accessories</Nav.Link>
+                        <Nav.Link as={NavLink} to="/sales">Sales</Nav.Link>
+                    </Nav>
+                </Navbar.Collapse>
             </Container>
         </Navbar>
     );
