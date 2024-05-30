a0671a0ea102581cb1fb4eb4170aeec1e21dc061
Julia Glinka <julia.w.glinka@gmail.com>
2024-05-30 10:22:41 +0200

    Render data from API in Home.js

--- client/src/components/pages/Home/Home.js
+++ client/src/components/pages/Home/Home.js
@@ -1,6 +1,24 @@
+import React, { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { loadProductsRequest, getProducts } from '../../../redux/productsRedux';
+
 const Home = props => {
+    const dispatch = useDispatch();
+    const products = useSelector(getProducts);
+  
+    useEffect(() => {
+      dispatch(loadProductsRequest());
+    }, [dispatch]);
+
     return(
-        <h1>Home</h1>
+        <div>
+            <h1>Products</h1>
+            <ul>
+                {products.map(product => (
+                <li key={product.id}>{product.name}</li>
+                ))}
+            </ul>
+        </div>
     );
 };
 
