a9dde973e27befa452ceaf72bf8f544ebad77476
Julia Glinka <julia.w.glinka@gmail.com>
2024-05-31 11:49:45 +0200

    Add express.static for /public to serve image datain main.ts

--- src/main.ts
+++ src/main.ts
@@ -2,12 +2,14 @@ import { NestFactory } from '@nestjs/core';
 import { ValidationPipe } from '@nestjs/common';
 import { AppModule } from './app.module';
 import * as cookieParser from 'cookie-parser';
+import * as express from 'express';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.use(cookieParser());
   app.useGlobalPipes(new ValidationPipe({ transform: true }));
   app.setGlobalPrefix('api');
+  app.use('/public', express.static('public'));
   await app.enableShutdownHooks();
   await app.listen(3000);
 }
