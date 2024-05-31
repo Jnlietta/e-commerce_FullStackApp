fbbe85eb732dec97ce5e9cfafa14f1c45112e0f7
Julia Glinka <julia.w.glinka@gmail.com>
2024-05-31 10:19:18 +0200

    Import UploadModule in products.module

--- src/products/products.module.ts
+++ src/products/products.module.ts
@@ -2,8 +2,10 @@ import { Module } from '@nestjs/common';
 import { ProductsController } from './products.controller';
 import { ProductsService } from './products.service';
 import { PrismaService } from 'src/shared/services/prisma.service';
+import { UploadModule } from 'src/upload/upload.module';
 
 @Module({
+  imports: [UploadModule],
   controllers: [ProductsController],
   providers: [ProductsService, PrismaService],
 })
