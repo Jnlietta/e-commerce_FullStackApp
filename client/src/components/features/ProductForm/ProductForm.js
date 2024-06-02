dd9a3bc226941061a25a3fbd05bac0e60baabece
Julia Glinka <julia.w.glinka@gmail.com>
2024-06-02 16:53:16 +0200

    Add validation of input value in ProductForm

--- client/src/components/features/ProductForm/ProductForm.js
+++ client/src/components/features/ProductForm/ProductForm.js
@@ -1,8 +1,19 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Form, InputGroup } from 'react-bootstrap';
 import styles from './ProductForm.module.scss';
 
 const ProductForm = ({ product }) => {
+  const [quantity, setQuantity] = useState(1);
+
+  const handleQuantityChange = (event) => {
+    const value = parseInt(event.target.value, 10); 
+    if (!isNaN(value) && Number.isInteger(value) && value > 0) {
+      // Check if the parsed value is a valid integer
+      setQuantity(value); // Set quantity state to the parsed value
+    } else {
+      setQuantity(1); // Set quantity state to default value if input is not a valid integer
+    }
+  };
 
   return (
     <div className={styles.productControls}>
@@ -27,10 +38,19 @@ const ProductForm = ({ product }) => {
           
         </Form.Control>
       </Form.Group>
+
       <Form.Group controlId="quantity">
         <Form.Label>Quantity</Form.Label>
         <InputGroup>
-          <Form.Control type="number" min="1" defaultValue="1" />
+        
+          <Form.Control 
+          type="number" 
+          min="1" 
+          defaultValue="1" 
+          value={quantity}
+          onChange={handleQuantityChange}
+          />
+
         </InputGroup>
       </Form.Group>
     </div>
