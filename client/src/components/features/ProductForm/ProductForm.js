fd2a72849555f688a58b79ddcb6907bb81a03175
Julia Glinka <julia.w.glinka@gmail.com>
2024-06-02 14:23:02 +0200

    Extract code of ProductForm.js and import it toProduct with new styles

--- client/src/components/features/ProductForm/ProductForm.js
+++ client/src/components/features/ProductForm/ProductForm.js
@@ -1,45 +1,50 @@
 import React from 'react';
-import ProductGallery from '../ProductGallery/ProductGallery';
-import { Container, Row, Col, Form, InputGroup } from 'react-bootstrap';
+import { Form, InputGroup } from 'react-bootstrap';
 import styles from './ProductForm.module.scss';
-
-
 
 const ProductForm = ({ product }) => {
 
-  if (!product) {
-    return <div>Product not found</div>;
-  }
-
   return (
-<Container className={styles.productContainer}>
-      <Row>
-        <Col xs={12} sm={6} className={styles.productImages}>
-          <ProductGallery product={product} />
-        </Col>
-        <Col md={6} className={styles.productInfo}>
-          <h1>{product.name}</h1>
-          <p>{product.shortDescription}</p>
-          <div className={styles.productControls}>
-            <Form.Group controlId="size">
-              <Form.Label>Size</Form.Label>
-              <Form.Control as="select">
-                <option value="one size">One Size</option>
-              </Form.Control>
-            </Form.Group>
-            <Form.Group controlId="quantity">
-              <Form.Label>Quantity</Form.Label>
-              <InputGroup>
-                <Form.Control type="number" min="1" defaultValue="1" />
-              </InputGroup>
-            </Form.Group>
-          </div>
-          <div className={styles.productDescription}>
-            <p>{product.description}</p>
-          </div>
-        </Col>
-      </Row>
-    </Container>
+    <div>
+      <h1 className={styles.productName}>{product.name}</h1>
+      <p className={styles.shortDescription}>{product.shortDescription}</p>
+      <div className={styles.priceInfo}>
+        <p><span>{product.price} $</span>VAT included</p>
+        <p>Lowest price in the past 30 days</p>
+      </div>
+      <div className={styles.productControls}>
+        <Form.Group controlId="size">
+          <Form.Label>Size</Form.Label>
+          <Form.Control as="select" style={{ width: '200px' }}>
+
+            {(product.name === 'Baseball Cap' || product.name === 'Sunglasses') && (
+              <option value="one size">One Size</option>
+            )}
+
+            {(product.name !== 'Baseball Cap' && product.name !== 'Sunglasses') && (
+              <>
+              <option value="XS">XS</option>
+              <option value="S">S</option>
+              <option value="M">M</option>
+              <option value="L">L</option>
+              <option value="XL">XL</option>
+              <option value="XXL">XXL</option>
+              </>
+            )}
+            
+          </Form.Control>
+        </Form.Group>
+        <Form.Group controlId="quantity">
+          <Form.Label>Quantity</Form.Label>
+          <InputGroup>
+            <Form.Control type="number" min="1" defaultValue="1" style={{ width: '200px', flex: 'none' }}/>
+          </InputGroup>
+        </Form.Group>
+      </div>
+      <div className={styles.productDescription}>
+        <p>{product.description}</p>
+      </div>
+    </div>
   );
 };
 
--- client/src/components/pages/Product/Product.js
+++ client/src/components/pages/Product/Product.js
@@ -1,15 +1,34 @@
 import ProductForm from "../../features/ProductForm/ProductForm";
+import ProductGallery from "../../features/ProductGallery/ProductGallery";
 import { useSelector } from 'react-redux';
-import { useParams } from 'react-router-dom';
+import { useNavigate, useParams } from 'react-router-dom';
 import { getProductById } from '../../../redux/productsRedux';
+import { Col, Container, Row } from "react-bootstrap";
+import styles from './Product.module.scss';
 
 const Product = props => {
-    const { id } = useParams();
-    const product = useSelector(state => getProductById(state, id));
+  const navigate = useNavigate();
+  const { id } = useParams();
+  const product = useSelector(state => getProductById(state, id));
 
-    return(
-        <ProductForm product={product} />
-    );
+  if (!product) {
+    navigate('/'); 
+    // navigate('/product/60bc81de-1d7f-4e7c-8666-216d80a27e2f');
+    return null;
+    }
+  
+  return (
+    <Container className={styles.productContainer}>
+      <Row>
+        <Col xs={12} sm={6} className={styles.productImages}>
+          <ProductGallery product={product} />
+        </Col>
+        <Col md={6} className={styles.productInfo}>
+          <ProductForm product={product} />
+        </Col>
+      </Row>
+    </Container>
+  );
 };
 
 export default Product;
