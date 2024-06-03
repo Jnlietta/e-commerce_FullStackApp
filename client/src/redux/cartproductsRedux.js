bdacb84c1ddf85fb5e5820bb5642b2388b5dffda
Julia Glinka <julia.w.glinka@gmail.com>
2024-06-03 08:34:15 +0200

    Import axios, API_URL, add selectors getCartProducts, getRequests, actions startRequest, endRequest, errorRequest, addToCart, thunk addToCartRequest in productsRedux.js

--- client/src/redux/cartproductsRedux.js
+++ client/src/redux/cartproductsRedux.js
@@ -1,5 +1,9 @@
-/* SELECTORS */
+import axios from 'axios';
+import { API_URL } from '../config';
 
+/* SELECTORS */
+export const getCartProducts = ({ cart }) => cart.data;
+export const getCartRequests = ({ cart }) => cart.requests;
 
 /* ACTIONS */
 
@@ -7,21 +11,52 @@
 const reducerName = 'cartproducts';
 const createActionName = name => `app/${reducerName}/${name}`;
 
+const START_REQUEST = createActionName('START_REQUEST');
+const END_REQUEST = createActionName('END_REQUEST');
+const ERROR_REQUEST = createActionName('ERROR_REQUEST');
+
+const ADD_TO_CART = createActionName('ADD_TO_CART');
+
+export const startRequest = payload => ({ payload, type: START_REQUEST });
+export const endRequest = payload => ({ payload, type: END_REQUEST });
+export const errorRequest = payload => ({ payload, type: ERROR_REQUEST });
+
+export const addToCart = payload => ({ payload, type: ADD_TO_CART });
+
 
 /* THUNKS */
-
+export const addToCartRequest = (cartProduct) => {
+  return async dispatch => {
+    dispatch(startRequest({ name: 'ADD_TO_CART' }));
+    try {
+      let res = await axios.post(`${API_URL}/cartproducts`, cartProduct);
+      dispatch(addToCart(res.data));
+      dispatch(endRequest({ name: 'ADD_TO_CART' }));
+    } catch(e) {
+      dispatch(errorRequest({ name: 'ADD_TO_CART', error: e.message }));
+    }
+  };
+};
 
 /* INITIAL STATE */
 
 const initialState = {
   data: [],
-  requests: [],
+  requests: {},
 };
 
 /* REDUCER */
 
 export default function reducer(statePart = initialState, action = {}) {
   switch (action.type) {
+    case ADD_TO_CART:
+      return { ...statePart, data: [...statePart.data, action.payload] };
+    case START_REQUEST:
+      return { ...statePart, requests: {...statePart.requests, [action.payload.name]: { pending: true, error: null, success: false }} };
+    case END_REQUEST:
+      return { ...statePart, requests: { ...statePart.requests, [action.payload.name]: { pending: false, error: null, success: true }} };
+    case ERROR_REQUEST:
+      return { ...statePart, requests: { ...statePart.requests, [action.payload.name]: { pending: false, error: action.payload.error, success: false }} };
     default:
       return statePart;
   }
