444daa26cb599a25d044cccae6457b69838ea853
Julia Glinka <julia.w.glinka@gmail.com>
2024-06-04 22:57:57 +0200

    Remove useless code from CartProducts.js

--- client/src/components/features/CartProducts/CartProducts.js
+++ client/src/components/features/CartProducts/CartProducts.js
@@ -1,15 +1,8 @@
-import { useSelector } from 'react-redux';
 import CartProductForm from '../CartProductForm/CartProductForm';
 import styles from './CartProducts.module.scss';
-import { ADD_TO_CART, getCartRequests } from '../../../redux/cartproductsRedux';
 
 const CartProducts = ({ cartProducts }) => {
-
-  const request = useSelector(state => getCartRequests(state, ADD_TO_CART));
-
-
-  if (request && request.pending) return <div>Loading...</div>;
-  else return (
+  return (
     <section>
       {cartProducts.map(cartProduct => 
           <div key={cartProduct.id} className={styles.cartProductFormContainer} >
