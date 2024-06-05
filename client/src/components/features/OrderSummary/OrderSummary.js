16b887eae26e07ba5dfd65fef6706a162a611b4d
Julia Glinka <julia.w.glinka@gmail.com>
2024-06-05 19:56:57 +0200

    Add styles to OrderSummary.js

--- client/src/components/features/OrderSummary/OrderSummary.js
+++ client/src/components/features/OrderSummary/OrderSummary.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Card, Row, Col, ListGroup } from 'react-bootstrap';
 import { IMAGES_URL } from '../../../config';
+import styles from './OrderSummary.module.scss';
 
 const OrderSummary = ({ cartProducts }) => {
     
@@ -10,17 +11,17 @@ const OrderSummary = ({ cartProducts }) => {
       <Card.Body>
         <ListGroup variant="flush">
             {cartProducts.map(cartProduct => (
-                <ListGroup.Item key={cartProduct.id}>
-                    <Row className="mb-3">
+                <ListGroup.Item key={cartProduct.id} className={styles.listGroup}>
+                    <Row className="mb-3" className={styles.cartFlex}>
                         <Col xs={4}>
-                        <img src={IMAGES_URL +cartProduct.product.mainImage} alt={cartProduct.product.name} style={{ width: '100%' }} />
+                        <img src={IMAGES_URL +cartProduct.product.mainImage} alt={cartProduct.product.name} style={{ width: '100%' }} className={styles.image}/>
                         </Col>
                         <Col xs={8}>
-                        <h5>{cartProduct.product.name}</h5>
+                        <h5 className={styles.productName}>{cartProduct.product.name}</h5>
                         <p>Size: {cartProduct.size}</p>
                         <p>Quantity: {cartProduct.quantity}</p>
                         {cartProduct.comment && <p>Comment: {cartProduct.comment}</p>}
-                        <p><strong>Price: {cartProduct.product.price.toFixed(2)} zł</strong></p>
+                        <p className={styles.price}><strong>Price: {cartProduct.product.price.toFixed(2)} zł</strong></p>
                         </Col>
                     </Row>
                 </ListGroup.Item>
