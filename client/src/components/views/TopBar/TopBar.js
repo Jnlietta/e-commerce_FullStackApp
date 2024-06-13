d9c78f5a20aa66f95994b936371b4646b0b8802b
Julia Glinka <julia.w.glinka@gmail.com>
2024-06-13 12:09:33 +0200

    Add button Search and handleClick func in TopBar

--- client/src/components/views/TopBar/TopBar.js
+++ client/src/components/views/TopBar/TopBar.js
@@ -2,35 +2,53 @@ import React from 'react';
 // import PropTypes from 'prop-types';
 
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faUser, faLock } from '@fortawesome/free-solid-svg-icons';
+import { faUser, faLock, faSearch } from '@fortawesome/free-solid-svg-icons';
 
 import styles from './TopBar.module.scss';
-import { Container, Nav } from 'react-bootstrap';
+import { Button, Container, Nav } from 'react-bootstrap';
 import { NavLink } from 'react-router-dom';
 import clsx from 'clsx';
+import { useDispatch, useSelector } from 'react-redux';
+import { getSearchStringVisibility, toggleSearchStringVisibility } from '../../../redux/searchStringRedux';
 
-const TopBar = ({ isLoggedIn }) => (
-  <Container>
-    <Nav className={'justify-content-end'}>
-        
-        {!isLoggedIn && 
-            <Nav.Link as={NavLink} to="/user/login" className={styles.links}>
-                <FontAwesomeIcon className={styles.icon} icon={faUser} /> Login
-            </Nav.Link>
-        }
-        {!isLoggedIn && 
-            <Nav.Link as={NavLink} to="/user/register" className={styles.links}>
-                <FontAwesomeIcon className={styles.icon} icon={faLock} /> Register
-            </Nav.Link>
-        }
-        {isLoggedIn &&
-            <Nav.Link as={NavLink} to="/user/logout" className={clsx(styles['links'], styles['linksLogout'])}>
-                <FontAwesomeIcon className={styles.icon} icon={faUser} /> Logout
-            </Nav.Link>
-        }
+const TopBar = ({ isLoggedIn }) => {
+    const dispatch = useDispatch();
+    const isVisible = useSelector(getSearchStringVisibility);
 
-    </Nav> 
-  </Container>
-);
+    const handleClick = () => {
+        console.log('Search clicked');
+
+        dispatch(toggleSearchStringVisibility())
+    }
+
+    console.log('isVisible', isVisible);
+
+    return (
+        <Container>
+            <Nav className={'justify-content-end'}>
+                
+                {!isLoggedIn && 
+                    <Nav.Link as={NavLink} to="/user/login" className={styles.links}>
+                        <FontAwesomeIcon className={styles.icon} icon={faUser} /> Login
+                    </Nav.Link>
+                }
+                {!isLoggedIn && 
+                    <Nav.Link as={NavLink} to="/user/register" className={styles.links}>
+                        <FontAwesomeIcon className={styles.icon} icon={faLock} /> Register
+                    </Nav.Link>
+                }
+                {isLoggedIn &&
+                    <Nav.Link as={NavLink} to="/user/logout" className={clsx(styles['links'], styles['linksLogout'])}>
+                        <FontAwesomeIcon className={styles.icon} icon={faUser} /> Logout
+                    </Nav.Link>
+                }
+
+                <Button variant="transparent" onClick={handleClick} className={styles.links}>
+                    <FontAwesomeIcon className={styles.icon} icon={faSearch} /> Search
+                </Button>
+            </Nav> 
+        </Container>
+    );
+};
 
 export default TopBar;
