62b3335389093c5762914f6cb486ffc247b67a0d
Julia Glinka <julia.w.glinka@gmail.com>
2024-06-13 12:08:36 +0200

    Add selector getSearchStringVisibility, action toggleSearchStringVisibility in searchStringRedux

--- client/src/redux/searchStringRedux.js
+++ client/src/redux/searchStringRedux.js
@@ -1,5 +1,6 @@
 /* SELECTORS */
 export const getSearchString = ({ searchString }) => searchString.data;
+export const getSearchStringVisibility = ({ searchString }) => searchString.visible;
 
 
 /* ACTIONS */
@@ -9,12 +10,15 @@ const reducerName = 'searchString';
 const createActionName = name => `app/${reducerName}/${name}`;
 
 const UPDATE_SEARCHSTRING = createActionName('UPDATE_SEARCHSTRING');
+const TOGGLE_SEARCHSTRING_VISIBILITY = createActionName('TOGGLE_SEARCHSTRING_VISIBILITY');
 
 export const updateSearchString = payload => ({ type: UPDATE_SEARCHSTRING, payload});
+export const toggleSearchStringVisibility = payload => ({ type: TOGGLE_SEARCHSTRING_VISIBILITY, payload });
 
 /* INITIAL STATE */
 const initialState = {
   data: '',
+  visible: false,
 };
 
 /* REDUCER */
@@ -22,7 +26,9 @@ const initialState = {
 export default function reducer(statePart = initialState, action = {}) {
   switch(action.type) {
     case UPDATE_SEARCHSTRING:
-      return { data: action.payload };
+      return { ...statePart, data: action.payload };
+    case TOGGLE_SEARCHSTRING_VISIBILITY:
+      return { ...statePart, visible: !statePart.visible };
     default:
       return statePart;
   };
