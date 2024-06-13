14238c8d05453ec96e0c96d1c75d2bedd4827542
Julia Glinka <julia.w.glinka@gmail.com>
2024-06-13 13:03:57 +0200

    Add case of visibility in SearchForm

--- client/src/components/features/SearchForm/SearchForm.js
+++ client/src/components/features/SearchForm/SearchForm.js
@@ -1,18 +1,19 @@
 import React from 'react';
 import { Form } from 'react-bootstrap';
-import { useDispatch } from 'react-redux';
-import { updateSearchString } from '../../../redux/searchStringRedux';
+import { useDispatch, useSelector } from 'react-redux';
+import { getSearchStringVisibility, updateSearchString } from '../../../redux/searchStringRedux';
 import styles from './SearchForm.module.scss';
 
 const SearchForm = ({ searchString }) => {
     const dispatch = useDispatch();
+    const visible = useSelector(getSearchStringVisibility);
 
     const handleChange = e => {
         dispatch(updateSearchString(e.target.value));
     };
 
     return (
-        <Form className={styles.form}>
+        <Form className={visible ? styles.form : styles.hidden}>
             <Form.Control
                 type="text"
                 value={searchString}
